1764951cfa949c031d4c97b26c87f501d0ef5635
Michaelis12 <squalboss@gmail.com>
2025-09-21 15:02:59 +0200

    test_local

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://mtg-spring.onrender.com",
+  baseURL: "http://localhost:8080",
+  //baseURL: "https://mtg-spring.onrender.com",
   withCredentials: true, // indispensable pour envoyer/recevoir cookies
   timeout: 30000,
   maxContentLength: 5 * 1024 * 1024,
