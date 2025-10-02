3927ab2a5f8fc4d8eb0c982d9679c512071598bd
Michaelis12 <squalboss@gmail.com>
2025-10-02 03:07:17 +0200

    debug card

--- src/pages/CardsDeckPage.js
+++ src/pages/CardsDeckPage.js
@@ -1017,7 +1017,7 @@ const CardsDeckPage = () => {
 
       // N'affiche pas les terrains de base
       const maskBaseLand = (value) => {
-        if(value < 7 ) {
+        if(value > 70 ) {
             return "none";
       }
     }
